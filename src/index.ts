f6b15fbcef837eab44fd3b8b6bc63e4aa526095b
Danilo Martinelli <danilomartinelli429@gmail.com>
2023-05-24 19:32:10 -0300

    feat: add isCnmValid

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import * as cdigit from "cdigit";
 
-const cnmGenerator = (cns: string, number: string) => {
+const cnmGenerator = (cns: string, number: string, book: string) => {
     const register = number.padStart(7, '0');
-    const book = "2";
     const preCnm = cns + book + register;
     const digit = cdigit.mod97_10.compute(preCnm);
 
-    return cns + "." + book + "." + number + "-" + digit;
+    return cns + "." + book + "." + register + "-" + digit;
 }
 
-const x = cnmGenerator("999999", "0000001");
+const x = cnmGenerator("999999", "103", "2");
+
+const isCnmValid = (cnm: string) =>  cdigit.mod97_10.validate(cnm);
 
 console.log(x);
+console.log(isCnmValid(x));
